7723b04478f8f7de3c0dc46663727378358de068
ArpitaMB <arpitambiradar3108@gmail.com>
2025-12-26 20:40:47 +0530

    Update test_cinema.py

--- test_cinema.py
+++ test_cinema.py
@@ -5,7 +5,7 @@ def test_booking_success():
     result = cinema_booking("Avengers", 50, 4, 150)
 
     assert "Booking successful" in result
-    assert "Total Price    : ₹600" in result
+    assert "Total Price    : Rs600" in result
     assert "Seats Left     : 46" in result
 
 
@@ -13,7 +13,7 @@ def test_seats_not_available():
     result = cinema_booking("Avengers", 10, 15, 150)
 
     assert "Seats not available" in result
-    assert "Total Price    : ₹0" in result
+    assert "Total Price    : Rs0" in result
     assert "Seats Left     : 10" in result
 
 
@@ -21,5 +21,5 @@ def test_exact_seat_booking():
     result = cinema_booking("Avatar", 5, 5, 200)
 
     assert "Booking successful" in result
-    assert "Total Price    : ₹1000" in result
+    assert "Total Price    : Rs1000" in result
     assert "Seats Left     : 0" in result
